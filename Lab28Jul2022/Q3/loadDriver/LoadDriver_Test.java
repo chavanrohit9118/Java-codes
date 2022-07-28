bf9984636d3718407d1dc9fee471dcc1399f3f5d
chavanrohit9118 <97946265+chavanrohit9118@users.noreply.github.com>
2022-07-28 22:09:46 +0530

    Add files via upload

---
+++ Lab28Jul2022/Q3/loadDriver/LoadDriver_Test.java
@@ -0,0 +1,34 @@
+package loadDriver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class LoadDriver_Test {
+
+	public static void main(String[] args) {
+		
+		
+		String url = "jdbc:mysql://localhost:3306/project1";
+		String driver = "com.mysql.cj.jdbc.Driver";
+		
+		Connection con=null;
+		
+		try {
+			Class.forName(driver);
+			System.out.println("Driver loaded ");
+			
+			con=DriverManager.getConnection(url,"root","Rohit@111ch");
+			System.out.println("Connection established");
+			
+			con.close();
+			
+		} catch (ClassNotFoundException | SQLException e) {
+			
+			e.printStackTrace();
+		}
+
+	}
+
+}
+
